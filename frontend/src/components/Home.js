6d800b8af49d946445c8041e42afcbf36bfb465b
Bhanu Partap Singh Rana <bhanurana3826@gmail.com>
2024-10-10 13:55:37 +0530

    Added hero section to homepage

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './Navbar'
+import HeroSection from './HeroSection'
 
 export default function Home() {
   return (
     <>
         <Navbar/>
+        <HeroSection/>
     </>
   )
 }
