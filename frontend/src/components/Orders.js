c7b9e65393dc633f98792a7d0dbf6ea02514d61e
Bhanu Partap Singh Rana <bhanurana3826@gmail.com>
2024-12-10 13:52:06 +0530

    updated styling in orders component

--- frontend/src/components/Orders.js
+++ frontend/src/components/Orders.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import styles from './Order.module.css'
+import styles from './Orders.module.css'
 import axios from 'axios';
 
 const Orders = () => {
